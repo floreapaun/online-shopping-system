4a0d39f2e1159667463f0b9214d2c66316602ead
serj99 <go_with_the_flow99@yahoo.com>
2019-11-28 14:55:18 +0200

    Show products by brand how it should.

--- action.php
+++ action.php
@@ -172,6 +172,7 @@ if(isset($_POST["get_seleted_Category"]) || isset($_POST["selectBrand"]) || isse
 		//exit($sql);
 	}
 	$run_query = mysqli_query($con,$sql);
+    $cnt = 0;
 	while($row=mysqli_fetch_array($run_query)){
 			$pro_id    = $row['product_id'];
 			$pro_cat   = $row['product_cat'];
@@ -180,43 +181,45 @@ if(isset($_POST["get_seleted_Category"]) || isset($_POST["selectBrand"]) || isse
 			$pro_price = $row['product_price'];
 			$pro_image = $row['product_image'];
             $cat_name = $row["cat_title"];
-			echo "
-					
-                        
-                        <div class='col-md-4 col-xs-6'>
-								<a href='product.php?p=$pro_id'><div class='product'>
-									<div class='product-img'>
-										<img  src='product_images/$pro_image'  style='max-height: 170px;' alt=''>
-										<div class='product-label'>
-											<span class='sale'>-30%</span>
-											<span class='new'>NEW</span>
-										</div>
-									</div></a>
-									<div class='product-body'>
-										<p class='product-category'>$cat_name</p>
-										<h3 class='product-name header-cart-item-name'><a href='product.php?p=$pro_id'>$pro_title</a></h3>
-										<h4 class='product-price header-cart-item-info'>$pro_price LEI</h4>
-										<div class='product-rating'>
-											<i class='fa fa-star'></i>
-											<i class='fa fa-star'></i>
-											<i class='fa fa-star'></i>
-											<i class='fa fa-star'></i>
-											<i class='fa fa-star'></i>
-										</div>
-										<div class='product-btns'>
-											<button class='add-to-wishlist' tabindex='0'><i class='fa fa-heart-o'></i><span class='tooltipp'>add to wishlist</span></button>
-											<button class='add-to-compare'><i class='fa fa-exchange'></i><span class='tooltipp'>add to compare</span></button>
-											<button class='quick-view' ><i class='fa fa-eye'></i><span class='tooltipp'>quick view</span></button>
-										</div>
-									</div>
-									<div class='add-to-cart'>
-										<button pid='$pro_id' id='product' href='#' tabindex='0' class='add-to-cart-btn'><i class='fa fa-shopping-cart'></i> add to cart</button>
-									</div>
-								</div>
-							</div>
-			";
-		}
-	}
+
+            if ($cnt == 0)
+                echo "<div class='row'>";
+
+            echo "
+                    <div class='col-md-4'>
+                      <a href='product.php?p=$pro_id'>
+                        <div class='product'>
+                          <div class='product-img'>
+                              <img  src='product_images/$pro_image' style='max-height: 170px;' alt=''>
+                          </div>
+                          <div class='product-body'>
+                              <p class='product-category'>$cat_name</p>
+                              <h3 class='product-name header-cart-item-name'>
+                                <a href='product.php?p=$pro_id'>$pro_title</a>
+                              </h3>
+                              <h4 class='product-price header-cart-item-info'>$pro_price LEI</h4>
+                          </div>
+                          <div class='add-to-cart'>
+                              <button pid='$pro_id' id='product' href='#' tabindex='0'
+                               class='add-to-cart-btn'><i class='fa fa-shopping-cart'></i>Adauga in cos</button>
+                          </div>
+                        </div>
+                      </a>
+                    </div>
+                 ";
+
+            $cnt++;
+            if ($cnt == 3) {
+                echo "</div>";
+                $cnt = 0;
+            }
+    }
+        
+    //close div if not closed
+    if ($cnt != 3) 
+        echo "</div>";
+    
+}
 	
 
 
