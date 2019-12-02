c9001a9eed55853cac94d1122a44b55d7c4a4f87
serj99 <go_with_the_flow99@yahoo.com>
2019-12-02 12:40:07 +0200

    Write to cookie on log out.

--- logout.php
+++ logout.php
@@ -6,12 +6,14 @@ unset($_SESSION["uid"]);
 
 unset($_SESSION["name"]);
 
+//user is logged out
+setcookie("auth", 0);
+
 $BackToMyPage = $_SERVER['HTTP_REFERER'];
 if(isset($BackToMyPage)) {
     header('Location: '.$BackToMyPage);
 } else {
     header('Location: index.php'); // default page
 }
-   
 
 ?>
