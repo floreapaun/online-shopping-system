544738162e464e92f5ae47d6ca0ea23cb59ed5ed
serj99 <go_with_the_flow99@yahoo.com>
2019-12-04 18:21:44 +0200

    Newsletter updated.

--- offersmail.php
+++ offersmail.php
@@ -8,7 +8,7 @@ if (isset($_POST["email"])) {
     if(empty($email)){
         echo "
 			<div class='alert alert-warning'>
-				<a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a><b>PLease Fill this field..!</b>
+				<a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a><b>Te rugam introdu mail-ul!</b>
 			</div>
 		";
 		exit();
@@ -17,7 +17,7 @@ if (isset($_POST["email"])) {
 		echo "
 			<div class='alert alert-warning'>
 				<a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
-				<b>this $email is not valid..!</b>
+				<b>Mail-ul $email nu este valid !</b>
 			</div>
 		";
 		exit();
@@ -29,7 +29,7 @@ if (isset($_POST["email"])) {
             echo "
                 <div class='alert alert-danger'>
                     <a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
-                    <b>Email Address is already available</b>
+                    <b>Mail-ul a mai fost folosit!</b>
                 </div>
             ";
             exit();
@@ -41,7 +41,7 @@ if (isset($_POST["email"])) {
             $run_query = mysqli_query($con,$sql);
                 echo "<div class='alert alert-success'>
                     <a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
-                    <b>Thanks for subscribing</b>
+                    <b>Multumim pentru abonare!</b>
                 </div>";
                 
                 
