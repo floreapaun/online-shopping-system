a0300f4cc61f84e08d5c41ce1b404fcd28b25afc
serj99 <go_with_the_flow99@yahoo.com>
2019-12-30 17:48:32 +0200

    Queries updated.

--- body.php
+++ body.php
@@ -1,4 +1,3 @@
-
    <div class="main main-raised">
 		<div class="container mainn-raised" style="width:100%;">
 
@@ -128,9 +127,10 @@
                     include 'db.php';
 								
                     
-					$product_query = "SELECT * FROM products 
-                                      INNER JOIN categories ON products.product_cat = categories.cat_id
-                                      ORDER BY product_id DESC LIMIT 3";
+                $product_query = "SELECT * FROM products 
+                                  INNER JOIN categories ON products.product_cat = categories.cat_id
+                                  ORDER BY product_id DESC LIMIT 3;";
+
                 $run_query = mysqli_query($con,$product_query);
                 if(mysqli_num_rows($run_query) > 0){
 
@@ -201,97 +201,27 @@
 						<div class="section-title">
 							<h3 class="title">Cele mai vandute</h3>
 							<div class="section-nav">
-                                <!--
-								<ul class="section-tab-nav tab-nav">
-									<li class="active"><a data-toggle="tab" href="#tab2">Formals</a></li>
-									<li><a data-toggle="tab" href="#tab2">Shirts</a></li>
-									<li><a data-toggle="tab" href="#tab2">T-Shirts</a></li>
-									<li><a data-toggle="tab" href="#tab2">Pants</a></li>
-								</ul>
-                                -->
 							</div>
 						</div>
 					</div>
 					<!-- /section title -->
 
-					<!-- Products tab & slick -->
+                    <!-- Products tab & slick -->
 					<div class="col-md-12 mainn mainn-raised">
 						<div class="row">
 							<div class="products-tabs">
 								<!-- tab -->
 								<div id="tab2" class="tab-pane fade in active">
 									<div class="products-slick" data-nav="#slick-nav-2">
-										<!-- product -->
-										<?php
-                    include 'db.php';
-								
-                    
-					$product_query = "SELECT * FROM products,categories WHERE product_cat=cat_id AND product_id BETWEEN 59 AND 65";
-                $run_query = mysqli_query($con,$product_query);
-                if(mysqli_num_rows($run_query) > 0){
-
-                    while($row = mysqli_fetch_array($run_query)){
-                        $pro_id    = $row['product_id'];
-                        $pro_cat   = $row['product_cat'];
-                        $pro_brand = $row['product_brand'];
-                        $pro_title = $row['product_title'];
-                        $pro_price = $row['product_price'];
-                        $pro_image = $row['product_image'];
-
-                        $cat_name = $row["cat_title"];
-
-                        echo "
-				
-                        
-                                
-								<div class='product'>
-									<a href='product.php?p=$pro_id'><div class='product-img'>
-										<img src='product_images/$pro_image' style='max-height: 170px;' alt=''>
-										<div class='product-label'>
-											<span class='sale'>-30%</span>
-											<span class='new'>NEW</span>
-										</div>
-									</div></a>
-									<div class='product-body'>
-										<p class='product-category'>$cat_name</p>
-										<h3 class='product-name header-cart-item-name'><a href='product.php?p=$pro_id'>$pro_title</a></h3>
-										<h4 class='product-price header-cart-item-info'>$pro_price LEI</h4>
-										<div class='product-rating'>
-											<i class='fa fa-star'></i>
-											<i class='fa fa-star'></i>
-											<i class='fa fa-star'></i>
-											<i class='fa fa-star'></i>
-											<i class='fa fa-star'></i>
-										</div>
-										<div class='product-btns'>
-											<button class='add-to-wishlist'><i class='fa fa-heart-o'></i><span class='tooltipp'>add to wishlist</span></button>
-											<button class='add-to-compare'><i class='fa fa-exchange'></i><span class='tooltipp'>add to compare</span></button>
-											<button class='quick-view'><i class='fa fa-eye'></i><span class='tooltipp'>quick view</span></button>
-										</div>
-									</div>
-									<div class='add-to-cart'>
-										<button pid='$pro_id' id='product' class='add-to-cart-btn block2-btn-towishlist' href='#'><i class='fa fa-shopping-cart'></i> add to cart</button>
-									</div>
-								</div>
-                               
-							
-                        
-			";
-		}
-        ;
-      
-}
-?>
-										
-										<!-- /product -->
-									</div>
-									<div id="slick-nav-2" class="products-slick-nav"></div>
+                                    </div>
+	                            <div id="slick-nav-2" class="products-slick-nav"></div>
 								</div>
 								<!-- /tab -->
 							</div>
 						</div>
 					</div>
 					<!-- /Products tab & slick -->
+
 				</div>
 				<!-- /row -->
 			</div>
@@ -326,12 +256,13 @@
 						<div class="products-widget-slick" data-nav="#slick-nav-3">
                             
                             <?php
-                                $query = "SELECT products.product_id, products.product_image, products.product_title, 
-                                         products.product_price FROM products 
-                                         JOIN orders ON orders.product_id = products.product_id
-                                         WHERE products.product_cat = 1
-                                         ORDER BY orders.qty DESC LIMIT 4;";
-                               
+
+                                $query = "SELECT order_products.product_id, SUM(order_products.qty), product_cat, 
+                                          product_image, product_title,  product_price FROM products 
+                                          JOIN order_products ON order_products.product_id = products.product_id
+                                          WHERE product_cat = 1
+                                          GROUP BY order_products.product_id ORDER BY SUM(order_products.qty) DESC LIMIT 4;";
+                            
                                 $result = mysqli_query($con, $query);
                                 while ($row = $result->fetch_array()){
                                     $cat1_prod_rows[] = $row;
@@ -393,11 +324,12 @@
 					</div>
 
                     <?php
-                        $query = "SELECT products.product_id, products.product_image, products.product_title, 
-                                 products.product_price FROM products 
-                                 JOIN orders ON orders.product_id = products.product_id
-                                 WHERE products.product_cat = 2
-                                 ORDER BY orders.qty DESC LIMIT 4;";
+
+                        $query = "SELECT order_products.product_id, SUM(order_products.qty), product_cat, 
+                                  product_image, product_title,  product_price FROM products 
+                                  JOIN order_products ON order_products.product_id = products.product_id
+                                  WHERE product_cat = 2
+                                  GROUP BY order_products.product_id ORDER BY SUM(order_products.qty) DESC LIMIT 4;";
                        
                         $result = mysqli_query($con, $query);
                         while ($row = $result->fetch_array()){
@@ -468,11 +400,12 @@
 					</div>
 
                     <?php
-                        $query = "SELECT products.product_id, products.product_image, products.product_title, 
-                                 products.product_price FROM products 
-                                 JOIN orders ON orders.product_id = products.product_id
-                                 WHERE products.product_cat = 3
-                                 ORDER BY orders.qty DESC LIMIT 4;";
+
+                        $query = "SELECT order_products.product_id, SUM(order_products.qty), product_cat, 
+                                  product_image, product_title,  product_price FROM products 
+                                  JOIN order_products ON order_products.product_id = products.product_id
+                                  WHERE product_cat = 3
+                                  GROUP BY order_products.product_id ORDER BY SUM(order_products.qty) DESC LIMIT 4;";
                        
                         $result = mysqli_query($con, $query);
                         while ($row = $result->fetch_array()){
