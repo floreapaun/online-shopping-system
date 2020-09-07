16b389005e68b5b008724cf232ed4f9eb3dd9312
serj99 <go_with_the_flow99@yahoo.com>
2020-09-07 08:39:53 +0300

    Show message if user not signed in when adding product to cart.

--- body.php
+++ body.php
@@ -1,49 +1,9 @@
    <div class="main main-raised">
 		<div class="container mainn-raised" style="width:100%;">
-
-			<!--
-			<div id="myCarousel" class="carousel slide" data-ride="carousel">
-				<div class="carousel-inner">
-					<div class="item active">
-						<img src="img/banner3.jpg" alt="Los Angeles" style="width:100%;">
-						
-					</div>
-
-					<div class="item">
-						<img src="img/banner2.jpg" style="width:100%;">
-						
-					</div>
-					
-					<div class="item">
-						<img src="img/banner4.jpg" alt="New York" style="width:100%;">
-						
-					</div>
-					<div class="item">
-						<img src="img/banner1.jpg" alt="New York" style="width:100%;">
-						
-					</div>
-					<div class="item">
-						<img src="img/banner3.jpg" alt="New York" style="width:100%;">
-						
-					</div>
-				</div>
-
-				<a class="left carousel-control _26sdfg" href="#myCarousel" data-slide="prev">
-				<span class="glyphicon glyphicon-chevron-left"></span>
-				<span class="sr-only" >Previous</span>
-				</a>
-				<a class="right carousel-control _26sdfg" href="#myCarousel" data-slide="next">
-				<span class="glyphicon glyphicon-chevron-right"></span>
-				<span class="sr-only">Next</span>
-				</a>
-			</div>
-			-->
 		</div>
-     
 
 		<!-- SECTION -->
 		<div class="section mainn mainn-raised">
-		
 		
 			<!-- container -->
 			<div class="container">
@@ -106,6 +66,10 @@
 			<div class="container">
 				<!-- row -->
 				<div class="row">
+
+                    <!-- here append message if product was added successfuly to cart or not -->
+                    <div class="col-md-12 col-xs-12" id="product_msg">
+					</div>
 
 					<!-- section title -->
 					<div class="col-md-12">
